666d803cc10c53676a9dfa0fbfca0aa1833a4dcf
Chiraggohil10~ <gohilchirag90994@gmail.com>
2024-12-18 01:17:24 +0530

    g

--- promise.js
+++ promise.js
@@ -1,13 +1,29 @@
 fetch('https://jsonplaceholder.typicode.com/photos')
-.then((res)=>{
-    res.json()
-    .then((response)=>{
-        console.log(response);
+    .then((res) => {
+        res.json()
+            .then((response) => {
+                displayPhotos(response);
+            })
+            .catch((error) => {
+                console.log(error);
+            })
     })
-    .catch((error)=>{
-        console.log(error);
+    .catch((err) => {
+        console.log(err);
     })
-})
-.catch((err)=>{
-    console.log(err);
-})
+
+
+// Function to display photos on the webpage
+function displayPhotos(response) {
+    let maindiv = document.getElementById('maindiv');
+    response.map((x) => {
+        let div = document.createElement('div');
+        div.classList.add('photo-item'); //apply to css
+
+        let images = document.createElement('img');
+        images.src = x.thumbnailUrl; // src of image
+
+        div.appendChild(images); //images store in div
+        maindiv.appendChild(div); //div stores in maindiv
+    });
+}
