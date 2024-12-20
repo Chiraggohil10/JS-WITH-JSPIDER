4bb9ab88d8cb74c9017102bb01397b6b951fd145
Chiraggohil10~ <gohilchirag90994@gmail.com>
2024-12-20 09:43:22 +0530

    d

--- up.js
+++ up.js
@@ -26,26 +26,33 @@ fetch('https://dummyjson.com/products')
 
 function display(resMessage) {
     let body = document.getElementById('jsp');
+
     let container = document.createElement('div');
     container.style.display = "grid";
     container.style.gridTemplateColumns = "250px 250px 250px 250px";
     container.style.justifyContent = "space-evenly";
-    for (let i = 0; i < resMessage.products.length; i++) {
+    
+    resMessage.products.map((product)=>{
+
         let data = document.createElement('div');
         data.style.height = "300px";
         data.style.width = "250px";
+
         let head = document.createElement('h1');
+
         let image = document.createElement('img');
-        head.innerHTML = resMessage.products[i].title;
+
+        head.innerHTML =product.title;
         head.style.fontSize = "15px";
-        image.src = resMessage.products[i].images[0];
+
+        image.src = product.images[0];
         image.style.height = "200px";
         image.style.width = "250px";
         image.style.border = "1px solid black";
+
         container.appendChild(data);
         data.appendChild(head);
         data.appendChild(image);
         body.appendChild(container);
-
-    }
+    })
 }
