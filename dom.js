62471aa788c6b1befa8404e835ecd4b7cfc3eca9
Chiraggohil10~ <gohilchirag90994@gmail.com>
2025-01-20 23:14:28 +0530

    dom update

--- dom.js
+++ dom.js
@@ -23,11 +23,17 @@
 let parent=document.getElementById("c1")
 console.log(parent);
 parent.innerHTML = "this is javascript"
+parent.style.backgroundColor="black"
+parent.style.color="aqua"
 
 let pera = document.getElementsByClassName("m1")
 console.log(pera);
 pera[0].innerHTML="div class"
 pera[1].innerHTML="peragraph class 2"
+Array.from(pera).forEach((x)=>{ //classname return htmlcollection so we need to convert it into array
+    x.style.color="red"
+})
+pera[0].style.color="blue"  // we can also change the style using this method but it is lengthy, if we have same style give to all element therefore foreach is easy
 
 let h3 = document.getElementsByTagName("h3")
 console.log(h3);
@@ -35,10 +41,16 @@ h3[0].innerHTML = "this is heading 1"
 h3[1].innerHTML = "this is heading 2"
 h3[2].innerHTML = "this is heading 3"
 h3[3].innerHTML = "this is heading 4"
+Array.from(h3).forEach((x)=>{ //tagname return htmlcollection so we need to convert it into array
+    x.style.color="red"
+})
+h3[0].style.color="blue"  // we can also change the style using this method but it is lengthy,if we have same style give to all element therefore foreach is easy
+
 
 let h1 = document.querySelector("#q1")
 console.log(h1);
 h1.textContent="query selector"
+h1.style.color="violet"
 
 let img = document.querySelectorAll("img")
 console.log(img);
@@ -73,6 +85,13 @@ img.forEach((x)=>{
 let d=document.getElementsByName("division")
 console.log(d);
 d[0].innerHTML="using name"
+d[1].innerHTML="using name"
+d[2].innerHTML="using name"
+d[3].innerHTML="using name"
+d.forEach((x)=>{  
+    x.style.color="red"
+})
+d[2].style.color="blue"
 
 let body = document.querySelector("body");
 body.style.transition="1500ms"
